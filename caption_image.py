dbdc9449a88e33cbd1c80a0ed1ae0e5e698631f1
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-07-04 12:07:44 -0400

    loop over image files and generate captions

--- caption_image.py
+++ caption_image.py
@@ -1,13 +1,17 @@
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from PIL import Image
 
-processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base", use_fast=True)
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
-img = Image.open("images/tweet_1919791768972693510_13_1915508916017868800.jpg")
 
-inputs = processor(img, return_tensors="pt")
-out = model.generate(**inputs)
-
-caption = processor.decode(out[0], skip_special_tokens=True)
-print(caption)
+with open("image_names.txt", "r") as f:
+    names = f.readlines()
+    for name in names:
+        img = Image.open(f"images/{name}".replace("\n", ""))
+        inputs = processor(img, return_tensors="pt")
+        out = model.generate(**inputs)
+        caption = processor.decode(out[0], skip_special_tokens=True)
+        print(name)
+        print(caption)
+        input()
