a33a46b82118d0a40322641aca46e55444a177b8
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-07-08 01:16:57 -0400

    basic tweet gathering working - text, images, image preview

--- gather_data.py
+++ gather_data.py
@@ -3,7 +3,9 @@ from pipes import quote
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 import tweepy
-import pickle
+import json
+
+OUTPUT_FILE_NAME = "bookmarked_tweets_v5.json"
 
 def get_twitter_client():
     client_id = os.environ.get("TWITTER_CLIENT_ID")
@@ -38,17 +40,37 @@ def fetch_bookmarked_tweets(client, pagination_token=None, batch_size=100):
 
 
 def get_quoted_data(quoted_tweet):
-    print(dir(quoted_tweet))
-    print(quoted_tweet.data)
-    print("items:", quoted_tweet.items())
-    print("keys:", quoted_tweet.keys())
-    print("values:", quoted_tweet.values())
-    input()
+    # print(dir(quoted_tweet))
+    # print(quoted_tweet.data)
+    # print("items:", quoted_tweet.items())
+    # print("keys:", quoted_tweet.keys())
+    # print("values:", quoted_tweet.values())
+    # input()
     return None, None
     # pass
 
-def save_tweet(t):
+def get_image_urls(t, includes_var):
+    if t.attachments is None:
+        return []
+
+    urls = []
+    media_keys = t.attachments.get('media_keys')
+    for item in includes_var.get('media', []):
+        if item.media_key in media_keys:
+            if item.preview_image_url:
+                urls.append(item.preview_image_url)
+            if item.url:
+                urls.append(item.url)
+    return urls
+
+def save_tweet(t, includes_var):
     base_text = t.text
+
+    base_image_urls = get_image_urls(t, includes_var)
+    print("base_text:", base_text)
+    print("base_image_urls:", base_image_urls)
+    input()
+
     if t.referenced_tweets and len(t.referenced_tweets) > 1:
         raise Exception(f"More than one tweet referenced: {t.referenced_tweets}")
 
@@ -59,6 +81,20 @@ def save_tweet(t):
         quoted_text = None
         quoted_images = []
 
+    with open(OUTPUT_FILE_NAME, "r") as f:
+        tweets_so_far = json.load(f)
+
+    new_tweet = {
+        "base_text": base_text,
+        "quoted_text": quoted_text,
+    }
+
+    tweets_so_far.append(new_tweet)
+    with open(OUTPUT_FILE_NAME, "w") as f:
+        json.dump(tweets_so_far,f, indent=2)
+
+
+
     
 
 
@@ -66,13 +102,8 @@ def main():
     client = get_twitter_client()
     bookmarked_tweets = fetch_bookmarked_tweets(client)
 
-    with open("foo.pkl", "wb") as f:
-        pickle.dump(bookmarked_tweets.data, f)
-
-    first_tweet = bookmarked_tweets.data[0]
-
     for tweet in bookmarked_tweets.data:
-        save_tweet(tweet)
+        save_tweet(tweet, bookmarked_tweets.includes)
     
 
     
