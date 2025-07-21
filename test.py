3fc2fdc6a5b002cc35200ebdd5875978d51f693a
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-07-21 14:32:40 -0700

    define TweetDataset class for pytorch

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
+import json
+
 import torch
+from torch.utils.data import Dataset
 from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
 
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
@@ -20,3 +23,31 @@ prediction = torch.argmax(logits, dim=1).item()
 print("Text:", text)
 print("logits:", logits)
 print("prediction:", prediction)
+
+class TweetDataset(Dataset):
+    def __init__(self, json_path, tokenizer, max_length=128):
+        with open(json_path, "r") as f:
+            self.data = json.load(f)
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        item = self.data[idx]
+        encoding = self.tokenizer(
+            item.get('text'),
+            truncation=True,
+            padding="max_length",
+            max_length=self.max_length,
+            return_tensors="pt"
+        )
+        encoding = {k: v.squeeze(0) for k, v in encoding.items()}
+        label = int(item.get('score'))
+        encoding['labels'] = torch.tensor(label)
+        return encoding
+
+
+dataset = TweetDataset('labeled_tweets.json', tokenizer)
+print(dataset[0])
