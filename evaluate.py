73783449f6f2fabe2fda0c89c4132b38757089bf
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-07-21 16:29:21 -0700

    run evaluation script in a loop to look at multiple outputs

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,7 @@
 import json
 
 import torch
+import torch.nn.functional as F
 from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
 
 
@@ -9,19 +10,28 @@ model_path = "bad_tweets_model_1"
 tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
 model = DistilBertForSequenceClassification.from_pretrained(model_path)
 
+device = torch.device("mps" if torch.backends.mps.is_available else "cpu")
+model = model.to(device)
+
 model.eval()
 
 with open("val.json", "r") as f:
     val_tweets = json.load(f)
 
-text = val_tweets[0].get('text')
-print(f'Text: {text}')
-inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True, max_length=128)
+for t in val_tweets:
+    text = t.get('text')
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True, max_length=128)
+    inputs = {k: v.to(device) for k, v in inputs.items()}
 
-with torch.no_grad():
-    outputs = model(**inputs)
-    logits = outputs.logits
-    prediction = torch.argmax(logits, dim=1).item()
+    with torch.no_grad():
+        outputs = model(**inputs)
+        logits = outputs.logits
+        prediction = torch.argmax(logits, dim=1).item()
+        probabilities = F.softmax(logits, dim=1)
 
-print(f"Prediction:", prediction)
-print(f"Logits: {logits}")
+    print("\n\n\n")
+    print(f'Begin Text:\n{text}\nEnd Text\n')
+    print(f"Prediction:", prediction)
+    print(f"Logits: {logits}")
+    print(f"Probabilities: {probabilities}")
+    input()
