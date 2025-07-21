bda354a3e424e54e87b2c14be1e4b079e06148a7
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-07-21 13:58:19 -0700

    switch to using Distilbert

--- test.py
+++ test.py
@@ -1,26 +1,22 @@
 import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
 
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 
-model_name = "elozano/bert-base-cased-clickbait-news"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+model_name = "distilbert-base-uncased"
+tokenizer = DistilBertTokenizer.from_pretrained(model_name)
+model = DistilBertForSequenceClassification.from_pretrained(model_name)
 
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
+text = "claude would definitely hang out in the English teachers classroom during lunch"
 
-model.to(device)
-model.eval()
-
-text = "This is fine"
-
-inputs = tokenizer(text, return_tensors="pt").to(device)
+inputs = tokenizer(text, return_tensors="pt")
 
 with torch.no_grad():
     outputs = model(**inputs)
     logits = outputs.logits
-    prediction = torch.argmax(logits, dim=1).item()
+
+prediction = torch.argmax(logits, dim=1).item()
 
 print("Text:", text)
 print("logits:", logits)
 print("prediction:", prediction)
-
