4154f9bb5ba7013fd3abbe0f57c8d2fca62c124f
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-09-20 23:58:15 -0700

    start writing training loop

--- python_files/train_clip_model.py
+++ python_files/train_clip_model.py
@@ -4,6 +4,8 @@ import torch.nn.functional as F
 import torch
 from Custom_CLIP_model import CustomCLIPModel
 
+
+torch.manual_seed(42)
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 
 clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
@@ -13,6 +15,7 @@ for param in clip_model.parameters():
     param.requires_grad = False
 
 myModel = CustomCLIPModel(clip_model, num_labels=3)
+myModel.to(device)
 
 
 trainable_params = sum(p.numel() for p in myModel.parameters() if p.requires_grad)
@@ -21,3 +24,11 @@ total_params = sum(p.numel() for p in myModel.parameters())
 print(f"Trainable parameters: {trainable_params:,}")
 print(f"Total parameters: {total_params:,}")
 print(f"Percentage trainable: {100 * trainable_params / total_params:.2f}%")
+
+loss_function = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(myModel.parameters(), lr=1e-3)
+
+
+EPOCHS = 10
+for epoch in range(0, EPOCHS):
+    pass
