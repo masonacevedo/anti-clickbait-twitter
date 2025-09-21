23f1ee4e1d981b5843345a1f806bc775efd73d8c
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-09-20 20:47:22 -0700

    use metal performance shaders

--- python_files/train_clip_model.py
+++ python_files/train_clip_model.py
@@ -3,18 +3,23 @@ from PIL import Image
 import torch.nn.functional as F
 import torch
 
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
 model.eval()
+model.to(device)
 
 image_path = "../images/GwOarDwWcAAOGL9.jpg"
 image = Image.open(image_path)
 
 image_inputs = processor(images=[image], return_tensors="pt")
+image_inputs.to(device)
 image_outputs = model.get_image_features(**image_inputs)
 
 text_inputs = processor(text="Zendaya hanging out with some friends.", return_tensors="pt")
+text_inputs.to(device)
 text_outputs = model.get_text_features(**text_inputs)
 
 
